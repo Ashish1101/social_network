5551270aba7865a29bf51a637807db497a81ad6a
Ashish <ashishskkumar321@gmail.com>
2021-01-26 14:46:42 +0530

    Post Schema Added

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    user : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    title: {
+        type:String,
+        required:true
+    },
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Comment"
+        }
+    ],
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+
+} , {timestamps:true})
+
+const Post = mongoose.model('Post' , postSchema);
+
+export default Post;
