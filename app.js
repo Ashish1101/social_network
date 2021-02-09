361b149b303ad87001971fcec0e07a1bfb0dfbb4
Ashish <ashishskkumar321@gmail.com>
2021-02-09 23:07:28 +0530

    cors policy fixed

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ mongoDb();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(morgan('dev'));
-app.use(cors())
+app.use(cors({
+   origin:'http://localhost:3000',
+   optionsSuccessStatus:200
+}))
 app.use(isAuth)
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
